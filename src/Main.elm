63f2f1681ba1a08cbd7ff00d653a177fe9c072bf
Steven Klaiber-Noble <steven.noble@gmail.com>
2020-12-08 13:50:37 -0800

    Update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -46,6 +46,15 @@ embeds =
             , attribute "allowfullscreen" "true"
             ]
             []
+          , iframe
+            [ style "width" "100%"
+            , style "height" "100%"
+            , src "https://www.youtube.com/embed/BeS46weU4ZI"
+            , style "frameborder" "0"
+            , attribute "allow" "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            , attribute "allowfullscreen" "true"
+            ]
+            []
         ]
     }
 
